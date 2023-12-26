486c4a13bcd7eea0e310e83efe10ba4a59016871
naskya <m@naskya.net>
2023-12-27 05:52:55 +0900

    Revert "chore: symlinc langmap to backend"
    This reverts commit 206d3c27f14571a485533eb2cf5f9cda68f2ceec.

--- packages/backend/src/misc/langmap.ts
+++ packages/backend/src/misc/langmap.ts
@@ -1,1 +1,382 @@
-packages/client/src/scripts/langmap.ts
+// TODO: sharedに置いてフロントエンドのと統合したい
+export const iso639Langs1 = {
+	af: {
+		nativeName: "Afrikaans",
+	},
+	ak: {
+		nativeName: "Tɕɥi",
+	},
+	ar: {
+		nativeName: "العربية",
+	},
+	ay: {
+		nativeName: "Aymar aru",
+	},
+	az: {
+		nativeName: "Azərbaycan dili",
+	},
+	be: {
+		nativeName: "Беларуская",
+	},
+	bg: {
+		nativeName: "Български",
+	},
+	bn: {
+		nativeName: "বাংলা",
+	},
+	br: {
+		nativeName: "Brezhoneg",
+	},
+	bs: {
+		nativeName: "Bosanski",
+	},
+	ca: {
+		nativeName: "Català",
+	},
+	cs: {
+		nativeName: "Čeština",
+	},
+	cy: {
+		nativeName: "Cymraeg",
+	},
+	da: {
+		nativeName: "Dansk",
+	},
+	de: {
+		nativeName: "Deutsch",
+	},
+	el: {
+		nativeName: "Ελληνικά",
+	},
+	en: {
+		nativeName: "English",
+	},
+	eo: {
+		nativeName: "Esperanto",
+	},
+	es: {
+		nativeName: "Español",
+	},
+	et: {
+		nativeName: "eesti keel",
+	},
+	eu: {
+		nativeName: "Euskara",
+	},
+	fa: {
+		nativeName: "فارسی",
+	},
+	ff: {
+		nativeName: "Fulah",
+	},
+	fi: {
+		nativeName: "Suomi",
+	},
+	fo: {
+		nativeName: "Føroyskt",
+	},
+	fr: {
+		nativeName: "Français",
+	},
+	fy: {
+		nativeName: "Frysk",
+	},
+	ga: {
+		nativeName: "Gaeilge",
+	},
+	gd: {
+		nativeName: "Gàidhlig",
+	},
+	gl: {
+		nativeName: "Galego",
+	},
+	gn: {
+		nativeName: "Avañe'ẽ",
+	},
+	gu: {
+		nativeName: "ગુજરાતી",
+	},
+	gv: {
+		nativeName: "Gaelg",
+	},
+	he: {
+		nativeName: "עברית‏",
+	},
+	hi: {
+		nativeName: "हिन्दी",
+	},
+	hr: {
+		nativeName: "Hrvatski",
+	},
+	ht: {
+		nativeName: "Kreyòl",
+	},
+	hu: {
+		nativeName: "Magyar",
+	},
+	hy: {
+		nativeName: "Հայերեն",
+	},
+	id: {
+		nativeName: "Bahasa Indonesia",
+	},
+	is: {
+		nativeName: "Íslenska",
+	},
+	it: {
+		nativeName: "Italiano",
+	},
+	ja: {
+		nativeName: "日本語",
+	},
+	jv: {
+		nativeName: "Basa Jawa",
+	},
+	ka: {
+		nativeName: "ქართული",
+	},
+	kk: {
+		nativeName: "Қазақша",
+	},
+	kl: {
+		nativeName: "kalaallisut",
+	},
+	km: {
+		nativeName: "ភាសាខ្មែរ",
+	},
+	kn: {
+		nativeName: "ಕನ್ನಡ",
+	},
+	ko: {
+		nativeName: "한국어",
+	},
+	ku: {
+		nativeName: "Kurdî",
+	},
+	kw: {
+		nativeName: "Kernewek",
+	},
+	la: {
+		nativeName: "Latin",
+	},
+	lb: {
+		nativeName: "Lëtzebuergesch",
+	},
+	li: {
+		nativeName: "Lèmbörgs",
+	},
+	lt: {
+		nativeName: "Lietuvių",
+	},
+	lv: {
+		nativeName: "Latviešu",
+	},
+	mg: {
+		nativeName: "Malagasy",
+	},
+	mk: {
+		nativeName: "Македонски",
+	},
+	ml: {
+		nativeName: "മലയാളം",
+	},
+	mn: {
+		nativeName: "Монгол",
+	},
+	mr: {
+		nativeName: "मराठी",
+	},
+	ms: {
+		nativeName: "Bahasa Melayu",
+	},
+	mt: {
+		nativeName: "Malti",
+	},
+	my: {
+		nativeName: "ဗမာစကာ",
+	},
+	no: {
+		nativeName: "Norsk",
+	},
+	nb: {
+		nativeName: "Norsk (bokmål)",
+	},
+	ne: {
+		nativeName: "नेपाली",
+	},
+	nl: {
+		nativeName: "Nederlands",
+	},
+	nn: {
+		nativeName: "Norsk (nynorsk)",
+	},
+	oc: {
+		nativeName: "Occitan",
+	},
+	or: {
+		nativeName: "ଓଡ଼ିଆ",
+	},
+	pa: {
+		nativeName: "ਪੰਜਾਬੀ",
+	},
+	pl: {
+		nativeName: "Polski",
+	},
+	ps: {
+		nativeName: "پښتو",
+	},
+	pt: {
+		nativeName: "Português",
+	},
+	qu: {
+		nativeName: "Qhichwa",
+	},
+	rm: {
+		nativeName: "Rumantsch",
+	},
+	ro: {
+		nativeName: "Română",
+	},
+	ru: {
+		nativeName: "Русский",
+	},
+	sa: {
+		nativeName: "संस्कृतम्",
+	},
+	se: {
+		nativeName: "Davvisámegiella",
+	},
+	sh: {
+		nativeName: "српскохрватски",
+	},
+	si: {
+		nativeName: "සිංහල",
+	},
+	sk: {
+		nativeName: "Slovenčina",
+	},
+	sl: {
+		nativeName: "Slovenščina",
+	},
+	so: {
+		nativeName: "Soomaaliga",
+	},
+	sq: {
+		nativeName: "Shqip",
+	},
+	sr: {
+		nativeName: "Српски",
+	},
+	su: {
+		nativeName: "Basa Sunda",
+	},
+	sv: {
+		nativeName: "Svenska",
+	},
+	sw: {
+		nativeName: "Kiswahili",
+	},
+	ta: {
+		nativeName: "தமிழ்",
+	},
+	te: {
+		nativeName: "తెలుగు",
+	},
+	tg: {
+		nativeName: "забо́ни тоҷикӣ́",
+	},
+	th: {
+		nativeName: "ภาษาไทย",
+	},
+	tr: {
+		nativeName: "Türkçe",
+	},
+	tt: {
+		nativeName: "татарча",
+	},
+	uk: {
+		nativeName: "Українська",
+	},
+	ur: {
+		nativeName: "اردو",
+	},
+	uz: {
+		nativeName: "O'zbek",
+	},
+	vi: {
+		nativeName: "Tiếng Việt",
+	},
+	xh: {
+		nativeName: "isiXhosa",
+	},
+	yi: {
+		nativeName: "ייִדיש",
+	},
+	zh: {
+		nativeName: "中文",
+	},
+	zu: {
+		nativeName: "isiZulu",
+	},
+};
+
+export const iso639Langs3 = {
+	ach: {
+		nativeName: "Lwo",
+	},
+	ady: {
+		nativeName: "Адыгэбзэ",
+	},
+	cak: {
+		nativeName: "Maya Kaqchikel",
+	},
+	chr: {
+		nativeName: "ᏣᎳᎩ (tsalagi)",
+	},
+	dsb: {
+		nativeName: "Dolnoserbšćina",
+	},
+	fil: {
+		nativeName: "Filipino",
+	},
+	hsb: {
+		nativeName: "Hornjoserbšćina",
+	},
+	kab: {
+		nativeName: "Taqbaylit",
+	},
+	mai: {
+		nativeName: "मैथिली, মৈথিলী",
+	},
+	tlh: {
+		nativeName: "tlhIngan-Hol",
+	},
+	tok: {
+		nativeName: "Toki Pona",
+	},
+};
+
+export const langmapNoRegion = Object.assign({}, iso639Langs1, iso639Langs3);
+
+export const iso639Regional = {
+	"zh-hans": {
+		nativeName: "中文（简体）",
+	},
+	"zh-hant": {
+		nativeName: "中文（繁體）",
+	},
+	"zh-cn": {
+		nativeName: "中文（中国大陆）",
+	},
+	"zh-hk": {
+		nativeName: "中文（香港）",
+	},
+	"zh-sg": {
+		nativeName: "中文（新加坡）",
+	},
+	"zh-tw": {
+		nativeName: "中文（台灣）",
+	},
+};
+
+export const langmap = Object.assign({}, langmapNoRegion, iso639Regional);
