ca67ca7bbeae959eb97f0f28072826e625904607
sup39 <dev@sup39.dev>
2024-01-25 09:43:19 +0900

    feat: use syntax highlight assets on server
    Co-authored-by: naskya <m@naskya.net>

--- packages/client/src/scripts/prism.ts
+++ packages/client/src/scripts/prism.ts
@@ -2,15 +2,13 @@ import Prism from "prismjs";
 import "prismjs/plugins/autoloader/prism-autoloader.js";
 import { defaultStore } from "@/store";
 
-// TODO
-Prism.plugins.autoloader.languages_path =
-	"https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/";
+Prism.plugins.autoloader.languages_path = defaultStore.state.useEmojiCdn
+	? "https://cdn.jsdelivr.net/npm/prismjs@1.29.0/components/"
+	: "/client-assets/prismjs/components/";
 
 const nonExistingLanguagesCache = new Set<string>();
 export const loadLanguage = (lang: string) =>
 	new Promise<void>((resolve, reject) => {
-		// for now
-		if (!defaultStore.state.useEmojiCdn) return reject();
 		// cached non-existing language
 		if (nonExistingLanguagesCache.has(lang)) return reject();
 		// load language with autoloader
