777a5a6bf77d7060892ccaebb13d24194fad4a22
naskya <m@naskya.net>
2024-02-11 02:33:16 +0900

    chore: fetch patrons from local file

--- packages/backend/src/server/api/endpoints/patrons.ts
+++ packages/backend/src/server/api/endpoints/patrons.ts
@@ -1,5 +1,4 @@
 import define from "@/server/api/define.js";
-import { redisClient } from "@/db/redis.js";
 import * as fs from "node:fs";
 import { fileURLToPath } from "node:url";
 import { dirname } from "node:path";
@@ -24,33 +23,9 @@ export const paramDef = {
 } as const;
 
 export default define(meta, paramDef, async (ps) => {
-	let patrons;
-	const cachedPatrons = await redisClient.get("patrons");
-	if (!ps.forceUpdate && cachedPatrons) {
-		patrons = JSON.parse(cachedPatrons);
-	} else {
-		AbortSignal.timeout ??= function timeout(ms) {
-			const ctrl = new AbortController();
-			setTimeout(() => ctrl.abort(), ms);
-			return ctrl.signal;
-		};
-
-		patrons = await fetch(
-			"https://git.joinfirefish.org/firefish/firefish/-/raw/develop/patrons.json",
-			{ signal: AbortSignal.timeout(2000) },
-		)
-			.then((response) => response.json())
-			.catch(() => {
-				const staticPatrons = JSON.parse(
-					fs.readFileSync(
-						`${_dirname}/../../../../../../patrons.json`,
-						"utf-8",
-					),
-				);
-				patrons = cachedPatrons ? JSON.parse(cachedPatrons) : staticPatrons;
-			});
-		await redisClient.set("patrons", JSON.stringify(patrons), "EX", 3600);
-	}
+	const patrons = JSON.parse(
+		fs.readFileSync(`${_dirname}/../../../../../../patrons.json`, "utf-8"),
+	);
 	return {
 		patrons: patrons["patrons"],
 		sponsors: patrons["sponsors"],
