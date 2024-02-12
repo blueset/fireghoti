b4c148a655ab0193ac68728dbacb3452c0d45933
naskya <m@naskya.net>
2024-02-12 23:31:04 +0900

    dev: build backend-rs and firefish-js first
    these packages are imported in other places

---
+++ scripts/build.mjs
@@ -0,0 +1,35 @@
+import path, { join } from "node:path";
+import { fileURLToPath } from "node:url";
+import { execa } from "execa";
+
+(async () => {
+	const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+	await execa(
+		"pnpm", [
+			"--recursive",
+			"--parallel",
+			"--filter=backend-rs",
+			"--filter=firefish-js",
+			"run",
+			"build",
+		], {
+			cwd: join(__dirname, "/../"),
+			stdio: "inherit",
+		}
+	);
+
+	await execa(
+		"pnpm",	[
+			"--recursive",
+			"--parallel",
+			"--filter=!backend-rs",
+			"--filter=!firefish-js",
+			"run",
+			"build",
+		], {
+			cwd: join(__dirname, "/../"),
+			stdio: "inherit",
+		}
+	);
+})();
