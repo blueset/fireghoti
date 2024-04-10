c1155f169a79d863aec38a99ef2ab34420d7dc77
Lhcfl <Lhcfl@outlook.com>
2024-04-10 20:42:48 +0800

    fix type warnings of MkCaptcha

--- packages/client/src/components/MkCaptcha.vue
+++ packages/client/src/components/MkCaptcha.vue
@@ -50,7 +50,7 @@ const props = defineProps<{
 }>();
 
 const emit = defineEmits<{
-	(ev: "update:modelValue", v: string | null): void;
+	"update:modelValue": [v: string | null];
 }>();
 
 const available = ref(false);
@@ -93,6 +93,7 @@ if (loaded) {
 				src: src.value,
 			}),
 		)
+	// biome-ignore lint/suspicious/noAssignInExpressions: assign it intentially
 	).addEventListener("load", () => (available.value = true));
 }
 
