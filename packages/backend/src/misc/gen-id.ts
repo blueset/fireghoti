ad30d16f23a6ee075cad4d5b5d9cbe89ecdd7643
naskya <m@naskya.net>
2024-04-12 06:36:17 +0900

    chore (backend): remove unused file

--- packages/backend/src/misc/gen-id.ts
+++
@@ -1,6 +0,0 @@
-import config from "@/config/index.js";
-import { initIdGenerator } from "backend-rs";
-
-const length = Math.min(Math.max(config.cuid?.length ?? 16, 16), 24);
-const fingerprint = config.cuid?.fingerprint ?? "";
-initIdGenerator(length, fingerprint);
