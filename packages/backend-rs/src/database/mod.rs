0cfa85197df4461294d8d01487ee3c1ee513b75b
sup39 <dev@sup39.dev>
2024-04-12 14:38:44 +0900

    refactor (backend-rs): rewrite the function to get db connection
    Co-authored-by: naskya <m@naskya.net>

--- packages/backend-rs/src/database/mod.rs
+++ packages/backend-rs/src/database/mod.rs
@@ -1,26 +1,37 @@
 pub mod error;
 
+use crate::config::server::SERVER_CONFIG;
 use error::Error;
 use sea_orm::{Database, DbConn};
 
 static DB_CONN: once_cell::sync::OnceCell<DbConn> = once_cell::sync::OnceCell::new();
 
-pub async fn init_database(conn_uri: impl Into<String>) -> Result<(), Error> {
-    let conn = Database::connect(conn_uri.into()).await?;
-    DB_CONN.get_or_init(move || conn);
-    Ok(())
+async fn init_database() -> Result<&'static DbConn, Error> {
+    let database_uri = format!(
+        "postgres://{}:{}@{}:{}/{}",
+        SERVER_CONFIG.db.user,
+        urlencoding::encode(&SERVER_CONFIG.db.pass),
+        SERVER_CONFIG.db.host,
+        SERVER_CONFIG.db.port,
+        SERVER_CONFIG.db.db,
+    );
+    let conn = Database::connect(database_uri).await?;
+    Ok(DB_CONN.get_or_init(move || conn))
 }
 
-pub fn get_database() -> Result<&'static DbConn, Error> {
-    DB_CONN.get().ok_or(Error::Uninitialized)
+pub async fn db_conn() -> Result<&'static DbConn, Error> {
+    match DB_CONN.get() {
+        Some(conn) => Ok(conn),
+        None => init_database().await,
+    }
 }
 
 #[cfg(test)]
 mod unit_test {
-    use super::{error::Error, get_database};
+    use super::db_conn;
 
-    #[test]
-    fn error_uninitialized() {
-        assert_eq!(get_database().unwrap_err(), Error::Uninitialized);
+    #[tokio::test]
+    async fn connect_test() {
+        assert!(db_conn().await.is_ok());
     }
 }
