cc37177bb55d765f74c69504b8cba976250f4757
naskya <m@naskya.net>
2024-04-24 13:43:18 +0900

    fix (backend-rs): use server's local time

--- packages/backend-rs/src/misc/remove_old_attestation_challenges.rs
+++ packages/backend-rs/src/misc/remove_old_attestation_challenges.rs
@@ -2,14 +2,14 @@
 
 use crate::database::db_conn;
 use crate::model::entity::attestation_challenge;
-use chrono::{Duration, Utc};
+use chrono::{Duration, Local};
 use sea_orm::{ColumnTrait, DbErr, EntityTrait, QueryFilter};
 
 /// Delete all entries in the "attestation_challenge" table created at more than 5 minutes ago
 #[crate::export]
 pub async fn remove_old_attestation_challenges() -> Result<(), DbErr> {
     attestation_challenge::Entity::delete_many()
-        .filter(attestation_challenge::Column::CreatedAt.lt(Utc::now() - Duration::minutes(5)))
+        .filter(attestation_challenge::Column::CreatedAt.lt(Local::now() - Duration::minutes(5)))
         .exec(db_conn().await?)
         .await?;
 
