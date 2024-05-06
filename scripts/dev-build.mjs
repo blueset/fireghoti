0c4826becf0bac7a0090f5b7c5243f3351b45699
naskya <m@naskya.net>
2024-05-06 22:54:10 +0900

    dev: copy backend-rs/index.{js,d.ts} to built/index.{js,d.ts} if not exist
    https://firefish.dev/firefish/firefish/-/merge_requests/10780#note_5685

--- scripts/dev-build.mjs
+++ scripts/dev-build.mjs
@@ -1,6 +1,7 @@
 import path, { join } from "node:path";
 import { fileURLToPath } from "node:url";
 import { execa } from "execa";
+import fs from "node:fs";
 
 (async () => {
 	const __dirname = path.dirname(fileURLToPath(import.meta.url));
@@ -32,4 +33,18 @@ import { execa } from "execa";
 			stdio: "inherit",
 		}
 	);
+
+	if (!fs.existsSync(join(__dirname, "/../packages/backend-rs/built/index.js"))) {
+		fs.copyFileSync(
+			join(__dirname, "/../packages/backend-rs/index.js"),
+			join(__dirname, "/../packages/backend-rs/built/index.js"),
+		);
+		console.warn("backend-rs/built/index.js has not been updated (https://github.com/napi-rs/napi-rs/issues/1768)");
+	}
+	if (!fs.existsSync(join(__dirname, "/../packages/backend-rs/built/index.d.ts"))) {
+		fs.copyFileSync(
+			join(__dirname, "/../packages/backend-rs/index.d.ts"),
+			join(__dirname, "/../packages/backend-rs/built/index.d.ts"),
+		);
+	}
 })();
