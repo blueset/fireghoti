cad3610d2ee06d11ea6b8d8857685229fd88f0d6
naskya <m@naskya.net>
2024-05-27 06:47:13 +0900

    fix: feather push notifications

--- packages/backend-rs/src/service/push_notification.rs
+++ packages/backend-rs/src/service/push_notification.rs
@@ -134,8 +134,8 @@ async fn encode_mastodon_payload(
         serde_json::to_value(token.token)?,
     );
 
-    // Ice Cubes and Mammoth expect notification_id to be an integer, but never use it.
-    if client.name == "IceCubesApp" || client.name == "Mammoth" {
+    // Ice Cubes, Mammoth, and feather expect notification_id to be an integer, but never use it.
+    if ["IceCubesApp", "Mammoth", "feather"].contains(&client.name.as_str()) {
         let timestamp = object
             .get("notification_id")
             .and_then(|id| id.as_str())
