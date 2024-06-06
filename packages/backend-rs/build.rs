64088819bea196e313eb58951f74d2e23c74d738
naskya <m@naskya.net>
2024-06-06 17:10:41 +0900

    chore (backend-rs): update build script

--- packages/backend-rs/build.rs
+++ packages/backend-rs/build.rs
@@ -1,9 +1,8 @@
-extern crate napi_build;
-
 fn main() {
     // watch the version in the project root package.json
     println!("cargo:rerun-if-changed=../../package.json");
 
     // napi
+    #[cfg(feature = "napi")]
     napi_build::setup();
 }
