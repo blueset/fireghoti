9a929b7a99f979d71fca88c3d6d6cc606f91a199
mia <mia@mia.jetzt>
2024-07-14 19:08:36 -0700

    fix (client): don't return undefined from native Mk:dialog implementation
    fixes #10839

--- packages/client/src/scripts/aiscript/api.ts
+++ packages/client/src/scripts/aiscript/api.ts
@@ -14,6 +14,7 @@ export function createAiScriptEnv(opts) {
 				title: title.value,
 				text: text.value,
 			});
+			return values.NULL;
 		}),
 		"Mk:confirm": values.FN_NATIVE(async ([title, text, type]) => {
 			const confirm = await os.confirm({
